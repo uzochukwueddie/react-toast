6059744cba328b30e33c99f1eebdaac3469dfffd
uzochukwueddie <uzo.odozi@gmail.com>
2020-03-25 09:35:53 +0100

    added auto dismiss functionality

--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -1,21 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import './Toast.css';
 
 const Toast = props => {
-    const { toastList, position } = props;
+    const { toastList, position, autoDelete, dismissTime } = props;
     const [list, setList] = useState(toastList);
+    const [toastDismiss, setToastDismiss] = useState([]); // add a new property to be used in auto-dismissing a toast
 
-    useEffect(() => {
-        setList(toastList);
-    }, [toastList, list]);
-
-    const deleteToast = id => {
+    // the useCallback is used so as to
+    // prevent unncessary renders
+    const deleteToast = useCallback(id => {
         const index = list.findIndex(e => e.id === id);
         list.splice(index, 1);
         setList([...list]);
-    }
+        
+        toastDismiss.length = 0; // after deleting a toast, clear the contents of the array
+        setToastDismiss([...toastDismiss]); // update the toastDismiss array with the empty array
+    }, [list, toastDismiss]);
+
+    useEffect(() => {
+        setList(toastList);
+        
+        // wait for some seconds before adding a toast to
+        // toastDismiss array
+        setTimeout(() => {
+            setToastDismiss(toastList);
+        }, dismissTime); // dismissTime is a prop passed into the toast component
+        
+        // eslint-disable-next-line
+    }, [toastList, list]);
+
+    // instead of using another useEffect, you can add its
+    // contents into the first useEffect hook
+    // it is just a choice for me to use a second useEffect hook
+    useEffect(() => {
+        const toast = toastDismiss.reverse(); // we only want the newly added toast inside the array. So we reverse to get the toast at index 0
+        // if autoDelete prop is true and toastDismiss length is > 0
+        if (autoDelete && toast.length) {
+            // wait for some seconds before auto deleting a toast
+            setTimeout(() => {
+                deleteToast(toast[0].id);
+            }, dismissTime);
+        }
+        
+        // eslint-disable-next-line
+    }, [toastDismiss]);
 
 
     return (
@@ -49,12 +79,15 @@ const Toast = props => {
 }
 
 Toast.defaultProps = {
-    position: 'bottom-right'
+    position: 'bottom-right',
+    dismissTime: 2000
 }
 
 Toast.propTypes = {
     toastList: PropTypes.array.isRequired,
-    position: PropTypes.string
+    position: PropTypes.string,
+    autoDelete: PropTypes.bool,
+    dismissTime: PropTypes.number
 }
 
 export default Toast;
